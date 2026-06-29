bb2a9b3ccaef0197fde45faad2f09016cd8e6cd3
Alex Room <alex.room@btinternet.com>
2026-06-29 13:42:52 +0100

    added filters to interface

--- src/interface.rs
+++ src/interface.rs
@@ -10,7 +10,7 @@ use crate::stats::Histogram;
 pub struct Data {
     dataset: NexusData,
     results: Histogram,
-    filters: Filters
+    filters: Filters,
 }
 
 #[pymethods]
@@ -21,7 +21,11 @@ impl Data {
     fn new(filename: String, n_spec: usize, chunk_size: usize) -> Result<Self> {
         let dataset = NexusData::new(filename, n_spec, chunk_size)?;
         let results = Histogram::new(0., 32.768, 2048);
-        Ok(Data { dataset, results, filters: Filters::new() })
+        Ok(Data {
+            dataset,
+            results,
+            filters: Filters::new(),
+        })
     }
 
     /// Calculate the histogram for the current data and filters.
@@ -48,5 +52,115 @@ impl Data {
     ///     The number of bins to divide the time range into.
     fn set_histogram_settings(&mut self, min_time: f32, max_time: f32, n_bins: usize) {
         self.results = Histogram::new(min_time, max_time, n_bins)
+    }
+
+    /// Set the type for the time filters.
+    ///
+    /// Parameters
+    /// ----------
+    /// filter_type: str
+    ///     The type for the time filters. Must be 'exclude' or 'include'.
+    fn set_time_type(&mut self, filter_type: String) -> Result<()> {
+        self.filters.set_time_type(filter_type)
+    }
+
+    /// Add a time filter.
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the time filter. Must be unique.
+    /// start: float
+    ///     The start point for the time filter.
+    /// end: float
+    ///     The end point for the time filter.
+    fn add_time_filter(&mut self, name: String, start: f64, end: f64) -> Result<()> {
+        self.filters.add_time_filter(name, start, end)
+    }
+
+    /// Remove a time filter.
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the time filter to remove.
+    fn remove_time_filter(&mut self, name: String) -> Result<()> {
+        self.filters.remove_time_filter(name)
+    }
+
+    /// Add a sample log filter.
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the log filter. Must be unique.
+    /// log: str
+    ///     The sample log in the data to which the filter applies.
+    /// lower: float
+    ///     The lower bound for the log filter.
+    /// upper: float
+    ///     The upper bound for the log filter.
+    fn add_log_filter(&mut self, name: String, log: String, lower: f64, upper: f64) -> Result<()> {
+        self.filters.add_log_filter(name, log, lower, upper)
+    }
+
+    /// Remove a sample log filter.
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the log filter to remove.
+    fn remove_log_filter(&mut self, name: String) -> Result<()> {
+        self.filters.remove_log_filter(name)
+    }
+
+    /// Add a sample log filter for all data above a certain value.
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the log filter. Must be unique.
+    /// log: str
+    ///     The sample log in the data to which the filter applies.
+    /// lower: float
+    ///     The lower bound for the log filter.
+    fn add_log_filter_above(&mut self, name: String, log: String, lower: f64) -> Result<()> {
+        self.filters.add_log_filter_above(name, log, lower)
+    }
+
+    /// Add a sample log filter for all data below a certain value.
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the log filter. Must be unique.
+    /// log: str
+    ///     The sample log in the data to which the filter applies.
+    /// upper: float
+    ///     The upper bound for the log filter.
+    fn add_log_filter_below(&mut self, name: String, log: String, upper: f64) -> Result<()> {
+        self.filters.add_log_filter_below(name, log, upper)
+    }
+
+    /// Set the amplitude filter for a detector.
+    ///
+    /// Parameters
+    /// ----------
+    /// detector: int
+    ///     The detector to set a filter for.
+    /// amp: float
+    ///     The maximum amplitude that should be ignored.
+    fn set_amp(&mut self, detector: usize, amp: f64) {
+        self.filters.set_amp(detector, amp)
+    }
+
+    /// Set an amplitude filter for all detectors that don't have one defined.
+    ///
+    /// Parameters
+    /// ----------
+    /// amp: float
+    ///     The maximum amplitude that should be ignored.
+    fn set_amps_baseline(&mut self, amp: f64) {
+        self.filters.set_amps_baseline(amp)
     }
 }
