ad88bd28e50a528cf61a4425c624eb13633b8919
Alex Room <alex.room@btinternet.com>
2026-07-13 14:50:37 +0100

    added amp histogram function

--- src/data/nexus_data.rs
+++ src/data/nexus_data.rs
@@ -1,13 +1,14 @@
+use std::cmp::min;
 use std::collections::HashMap;
 use std::path::Path;
 
 use anyhow::{Error, Result};
 use hdf5::{Dataset, File, Group};
-use ndarray::Array1;
+use ndarray::{s, Array1};
 use numpy::{PyArray1, ToPyArray};
 use pyo3::prelude::{pyclass, pymethods, Bound, PyDictMethods, Python};
 use pyo3::types::PyDict;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 
 use crate::data::SampleLog;
 
@@ -57,7 +58,7 @@ impl NexusData {
         let sample_log = self.get_sample_log(&log_name)?;
 
         let output = PyDict::new(py);
-        // pydict is dynamically typed (as it's a Python object),
+        // PyDict is dynamically typed (as it's a Python object),
         // but we need to unpack the type of the SampleLog anyway to make the Rust compiler happy
         match sample_log {
             SampleLog::Float(log) => {
@@ -70,6 +71,18 @@ impl NexusData {
             }
         };
         Ok(output)
+    }
+
+    /// Get a histogram of the amplitudes and the maximum height.
+    #[pyo3(name = "get_amp_histogram", signature = (max_height=None, n_bins=10))]
+    fn get_amp_histogram_py<'py>(
+        &self,
+        py: Python<'py>,
+        max_height: Option<f64>,
+        n_bins: usize,
+    ) -> Result<(Bound<'py, PyArray1<usize>>, f64)> {
+        let (results, max) = self.get_amp_histogram(max_height, n_bins)?;
+        Ok((results.to_pyarray(py), max))
     }
 }
 
@@ -93,6 +106,78 @@ impl NexusData {
             .into_par_iter()
             .map(|name| self.get_sample_log(&name).map(|log| (name, log)))
             .collect()
+    }
+
+    /// Get the histogram of amplitudes and the max height.
+    fn get_amp_histogram(
+        &self,
+        max_height: Option<f64>,
+        n_bins: usize,
+    ) -> Result<(Array1<usize>, f64)> {
+        let max = match max_height {
+            Some(height) => height,
+            None => self.get_dataset_max(&self.amps)?,
+        };
+        let width = max / n_bins as f64;
+
+        // parallel iterate over chunks
+        let results = Array1::from_iter(
+            (0..self.n_events)
+                .into_par_iter()
+                .step_by(self.chunk_size)
+                // get the amps for each chunk
+                .map(|start| -> Array1<f64> {
+                    let end = min(start + self.chunk_size, self.n_events);
+                    let array_slice = s![start..end];
+                    self.amps
+                        .read_slice_1d(array_slice)
+                        .expect("Failed to read amplitude data.")
+                })
+                // bin amps for each chunk
+                .map(|amps| {
+                    let mut array = Array1::zeros(n_bins);
+                    for amp in amps {
+                        // anything over the max is put in the last bin
+                        if amp >= max {
+                            array[n_bins - 1] += 1
+                        } else {
+                            let bin = (amp / width).floor() as usize;
+                            array[bin] += 1
+                        }
+                    }
+                    array
+                })
+                // combine chunks
+                .reduce(
+                    // rayon's reduce requires an identity value
+                    || Array1::zeros(n_bins),
+                    |mut acc, r| {
+                        acc += &r;
+                        acc
+                    },
+                ),
+        );
+        Ok((results, max))
+    }
+
+    /// Get the maximum value of a dataset.
+    fn get_dataset_max(&self, dataset: &Dataset) -> Result<f64> {
+        Ok((0..self.n_events)
+            .into_par_iter()
+            .step_by(self.chunk_size)
+            .map(|start| -> f64 {
+                let end = min(start + self.chunk_size, self.n_events);
+                let array_slice = s![start..end];
+                let array: Array1<f64> = dataset
+                    .read_slice_1d(array_slice)
+                    .expect("Failed to read amplitude data.");
+                array
+                    .into_iter()
+                    .max_by(|a, b| a.partial_cmp(b).unwrap())
+                    .unwrap()
+            })
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap())
     }
 }
 
@@ -202,5 +287,14 @@ mod tests {
 
         let log = data.get_sample_log(&"Lunch".to_string());
         assert!(log.is_err())
+    }
+
+    /// Test that an amplitude histogram is successfully created for some data.
+    #[test]
+    fn test_make_amp_histogram() {
+        let data = test_data();
+
+        let result = data.get_amp_histogram(None, 10);
+        assert!(result.is_ok())
     }
 }
