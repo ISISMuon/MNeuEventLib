0173d33e376a4bdb8649d370081788b8acf15339
Alex Room <alex.room@btinternet.com>
2026-07-14 10:11:27 +0100

    added amp histogram plotting

--- MNeuEventLib/plotting.py
+++ MNeuEventLib/plotting.py
@@ -1,6 +1,8 @@
 """Plotting functions for data."""
+from typing import Optional
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from MNeuEventLib.core import Data
 
@@ -20,3 +22,26 @@ def plot_sample_log(data: Data, log_name: str):
     ax.plot(sample_log['time'], sample_log['value'])
 
     plt.show()
+
+def plot_amplitudes(data: Data, max_height: Optional[float] = None, n_bins: int = 10):
+    """
+    Plot a histogram of amplitudes.
+
+    Parameters
+    ----------
+    data: Data
+        The Data object containing the sample log.
+    max_height: float, optional
+        The maximum height to use.
+        Any data above this height will be placed in the last bin.
+        If not provided, calculates the maximum amplitude in the data.
+    n_bins: int, default 10
+        The number of equal-width bins to use.
+    """
+    # get_amp_histogram returns the histogram and the max height used
+    hist, max_h = data.dataset.get_amp_histogram(max_height, n_bins)
+
+    steps = np.linspace(0, max_h, len(hist)+1)
+    fig, ax = plt.subplots()
+    ax.stairs(hist, steps)
+    
