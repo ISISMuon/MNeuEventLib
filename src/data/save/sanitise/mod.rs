5b01b873891ec0993dd8eef545d448864ce0f638
Anthony Lim <anthony.lim@stfc.ac.uk>
2026-09-15 13:13:59 +0100

    change copy attribute to not use raw hdf5

--- src/data/save/sanitise/mod.rs
+++ src/data/save/sanitise/mod.rs
@@ -1,3 +1,8 @@
-//! Code for saving outputs to file.
+//! Code for saving outputs to files that
+//! are compatible with Mantid.
+//! This is done by filling in the missing metadata
+//! in the output file based on a reference file.
+//! This allows the file to be read by Mantid even if the
+//! event file is incomplete.
 pub mod nexus_data;
 pub mod utils;
