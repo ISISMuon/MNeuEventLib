58e376d46c1b9f1ca268349a62dac6cc1770f2cc
Anthony <anthony.lim@stfc.ac.uk>
2026-09-21 10:58:47 +0100

    fix deleting wrong name

--- tests/integration/test_mantid.py
+++ tests/integration/test_mantid.py
@@ -246,7 +246,7 @@ def test_mantid_workflow_Load_batch_multi():
         result_k = Load(Filename=hist_file_k, deadtimeTable='deadtimes')
         mantid_workflow(result_k, periods)
         os.remove(hist_file_k)
-    os.remove(event_file)
+    os.remove(file)
 
 def test_mantid_workflow_LoadMuonNexusv2_batch_multi():
     """
@@ -279,7 +279,7 @@ def test_mantid_workflow_LoadMuonNexusv2_batch_multi():
         result_k = LoadMuonNexusV2(Filename=hist_file_k, deadtimeTable='deadtimes')
         mantid_workflow(result_k, periods)
         os.remove(hist_file_k)
-    os.remove(event_file)
+    os.remove(file)
 
 def test_mantid_workflow_Load_batch_single():
     """
